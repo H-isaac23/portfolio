c3444e09a046dff614e3c245603055fdff390f48
H-isaac23 <dev.isaac23@gmail.com>
2024-08-06 19:27:45 +0800

    Remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import MainLayout from "./components/MainLayout/MainLayout";
-import Projects from "./components/Projects/Projects";
-import Home from "./components/Home/Home";
-import Contact from "./components/Contact/Contact";
 import { Outlet } from "react-router-dom";
 import "./App.css";
 
@@ -10,10 +7,6 @@ function App() {
   return (
     <>
       <MainLayout>
-        {/* <p>some paragraph here</p> */}
-        {/* <Projects /> */}
-        {/* <Home /> */}
-        {/* <Contact /> */}
         <Outlet />
       </MainLayout>
     </>
