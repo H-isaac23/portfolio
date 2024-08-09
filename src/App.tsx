f95e6d248a8f05736353d12bc6066f49cf9ee18d
H-isaac23 <dev.isaac23@gmail.com>
2024-08-09 10:23:16 +0800

    Animate components on exit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import MainLayout from "./components/MainLayout/MainLayout";
-import { Outlet } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
+import AnimatedOutlet from "./components/AnimatedOutlet/AnimatedOutlet";
 import "./App.css";
 
 function App() {
+  const { pathname } = useLocation();
   return (
     <>
       <MainLayout>
-        <Outlet />
+        <AnimatePresence mode="wait">
+          <motion.div
+            className="reset-container"
+            key={pathname}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <AnimatedOutlet />
+          </motion.div>
+        </AnimatePresence>
       </MainLayout>
     </>
   );
